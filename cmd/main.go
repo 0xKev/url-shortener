aeb24f7818fd54b99f03378258cde46c8b2173a5
0xKev <56137695+0xKev@users.noreply.github.com>
2024-09-12 18:33:01 -0400

    simple main.go working

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,21 @@ import (
 	"log"
 	"net/http"
 
-	server "github.com/0xKev/url-shortener/internal/server"
+	"github.com/0xKev/url-shortener/internal/base62"
+	"github.com/0xKev/url-shortener/internal/server"
+	"github.com/0xKev/url-shortener/internal/shortener"
 	store "github.com/0xKev/url-shortener/internal/store"
 )
 
+type EncoderFunc func(num uint64) string
+
+func (e EncoderFunc) Encode(num uint64) string {
+	return e(num)
+}
+
+var encoder shortener.Encoder = EncoderFunc(base62.Encode)
+
 func main() {
-	shortenerServer := server.NewURLShortenerServer(store.NewInMemoryURLStore())
+	shortenerServer := server.NewURLShortenerServer(store.NewInMemoryURLStore(), shortener.NewURLShortener(nil, encoder))
 	log.Fatal(http.ListenAndServe(":5000", shortenerServer))
 }
