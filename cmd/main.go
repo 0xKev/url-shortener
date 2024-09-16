886a46d82e78557541a270d079dc25ff7c45bb21
0xKev <56137695+0xKev@users.noreply.github.com>
2024-09-16 15:37:55 -0400

    updated main to use redis store

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/0xKev/url-shortener/internal/base62"
 	"github.com/0xKev/url-shortener/internal/server"
 	"github.com/0xKev/url-shortener/internal/shortener"
-	store "github.com/0xKev/url-shortener/internal/store/memory"
+	redisStore "github.com/0xKev/url-shortener/internal/store/redis"
 )
 
 type EncoderFunc func(num uint64) string
@@ -18,7 +18,18 @@ func (e EncoderFunc) Encode(num uint64) string {
 
 var encoder shortener.Encoder = EncoderFunc(base62.Encode)
 
+const (
+	redisAddr = "localhost:6379"
+	redisPass = ""
+	redisDB   = 0
+)
+
 func main() {
-	shortenerServer := server.NewURLShortenerServer(store.NewInMemoryURLStore(), shortener.NewURLShortener(nil, encoder))
+	storeConfig := redisStore.NewRedisConfig(redisAddr, redisAddr, redisDB)
+	store, err := redisStore.NewRedisURLStore(storeConfig)
+	if err != nil {
+		log.Fatalf("error when creating redis store %v", err)
+	}
+	shortenerServer := server.NewURLShortenerServer(store, shortener.NewURLShortener(nil, encoder))
 	log.Fatal(http.ListenAndServe(":5000", shortenerServer))
 }
